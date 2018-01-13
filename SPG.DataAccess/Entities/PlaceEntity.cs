f316ab7e5215981ed25f200d70289533be4231b9
Mart0g <bad_boy95@abv.bg>
2018-01-13 18:38:53 +0200

    Added connection to database and fixed nullable fields

--- SPG.DataAccess/Entities/PlaceEntity.cs
+++ SPG.DataAccess/Entities/PlaceEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace SPG.DataAccess.Entities
     public class PlaceEntity
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public PlaceCategoryEntity Category { get; set; }
         public SubCategoryEntity SubCategory { get; set; }
     }
