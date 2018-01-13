33d6853e1640d74d9b93dfc649e60651954108a0
Mart0g <bad_boy95@abv.bg>
2018-01-13 18:02:58 +0200

    Updated styles

--- SPG/Controllers/HomeController.cs
+++ SPG/Controllers/HomeController.cs
@@ -14,5 +14,11 @@ namespace SPG.Controllers
 
             return View();
         }
+        public ActionResult Chat()
+        {
+            ViewBag.Title = "Chat";
+
+            return View();
+        }
     }
 }
