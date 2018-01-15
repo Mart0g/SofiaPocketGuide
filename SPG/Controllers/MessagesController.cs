83851bc0bf6837b9bb5de8b2862c60e6ba8c2a84
Mart0g <bad_boy95@abv.bg>
2018-01-15 02:50:29 +0200

    Added controller for user messages.

---
+++ SPG/Controllers/MessagesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SPG.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public ActionResult Message(String message)
+        {
+            ViewBag.Question = message;
+
+            return View(message);
+        }
+	}
+}
