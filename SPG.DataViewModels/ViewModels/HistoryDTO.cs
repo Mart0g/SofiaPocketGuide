f1711543b8cff5a914dd2d2d7b2c5663087cccb3
Mart0g <bad_boy95@abv.bg>
2018-02-07 00:42:27 +0200

    Made chat layout.

---
+++ SPG.DataViewModels/ViewModels/HistoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPG.DataViewModels.ViewModels
+{
+    public class HistoryDTO
+    {
+        public List<MessageDTO> History { get; set; }
+
+        public MessageDTO Current { get; set; }
+    }
+}
--- SPG/Controllers/MessageController.cs
+++ SPG/Controllers/MessageController.cs
@@ -10,32 +10,34 @@ namespace SPG.Controllers
 {
     public class MessageController : Controller
     {
-        private readonly MessageService chatService;
-
+        private readonly MessageService messageService;
+        List<MessageDTO> messages = new List<MessageDTO>();
+        private static HistoryDTO history = new HistoryDTO();
         public MessageController()
         {
-            this.chatService = new MessageService(new DataAccess.DataAccessService());
+            this.messageService = new MessageService(new DataAccess.DataAccessService());
         }
 
-        public MessageController(MessageService chatService)
+        public MessageController(MessageService messageService)
         {
-            this.chatService = chatService;
+            this.messageService = messageService;
         }   
 
         public ActionResult Message()
         {
-            return View();
+            history.History = new List<MessageDTO>();
+            history.Current = new MessageDTO();
+            history.History.AddRange(messages);
+            return View(history);
         }
 
         [HttpPost]
-        public ActionResult SendMessage(MessageDTO model)
+        public ActionResult ShowMessage(HistoryDTO model)
         {
-            if (ModelState.IsValid)
-            {
-                //TODO: SubscribeUser(model.Email);
-            }
-
-            return View("Message", model);
+            history.History.Add(model.Current);
+            history.Current = new MessageDTO();
+            ModelState.Clear();
+            return View("Message", history);
         }
 	}
 }
